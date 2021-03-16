4d91f10d0d809739e5fb1f38c8c6962fbcd55828
klay2000 <motoklay@gmail.com>
2021-03-16 14:10:28 -0400

    finished pygame boilerplate

--- agagla/__main__.py
+++ agagla/__main__.py
@@ -1,8 +1,18 @@
 from game_state_manager import *
+import pygame
 
-gsm = GameStateManager()
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
 
 if __name__ == '__main__':
 
+    gsm = GameStateManager()
+
+    pygame.init()
+    pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.FULLSCREEN)
+
     while gsm.get_state() != GameState.exit:
         gsm.game_loop()
+        pygame.display.update()
+
+    pygame.quit()
