6a5bbeb74fa9070bce65f04ef739a84e650275a0
klay2000 <motoklay@gmail.com>
2021-03-16 12:05:31 -0400

    wip

--- src/game_state_manager.py
+++ src/game_state_manager.py
@@ -1,4 +1,5 @@
 import enum
+import time
 
 class GameState(enum.Enum):
     menu = 1
@@ -7,7 +8,93 @@ class GameState(enum.Enum):
 
 class GameStateManager:
     def __init__(self):
-        self.state = GameState.menu
+        self.tick_rate = 60
+        self._last_game_state = None
+        self._current_game_state = GameState.menu
+        self.game_score = 0
+        self.lives = 0
+        self.entities = []
+        self._last_tick_time = time.time()
+        self.states_switch = {GameState.menu: self._menu_fn,
+                              GameState.running: self._running_fn,
+                              GameState.game_over: self._game_over_fn}
+
+    def _set_state(self, state):
+        self._current_game_state = state
+
+    def get_state(self):
+        return self._current_game_state
+
+    def get_enemies(self):
+        enemies = []
+
+        for i in self.entities:
+            if type(i).__name__ == 'Enemy':
+                enemies.append(i)
+
+        return enemies
+
+    def get_projectiles(self):
+        projectiles = []
+
+        for i in self.entities:
+            if type(i).__name__ == 'Projectile':
+                projectiles.append(i)
+
+            return projectiles
+
+    def get_player_ship(self):
+        for i in self.entities:
+            if type(i).__name__ == 'Player_Ship':
+                return i
+
+        return None
+
+    def get_ships(self):
+        return self.get_enemies().append(self.get_player_ship())
+
+    def get_entities(self):
+        return self.entities
+
+    def add_entity(self, e):
+        self.entities.append(e)
+
+    def get_score(self):
+        return self.game_score
+
+    def start_game(self):
+        self._set_state(GameState.running)
+
+    def _menu_fn(self, initial_run):
+        return None
+
+    def _running_fn(self, initial_run):
+        if self.lives <= 0:
+            self._set_state(GameState.game_over)
+
+        self._tick()
+        self._render_game()
+
+    def _game_over_fn(self, initial_run):
+        return None
 
     def _game_loop(self):
+        self.states_switch[self._current_game_state](not self._current_game_state == self._last_game_state)
+        self._last_game_state = self._current_game_state
 
+    def _tick(self):
+        if time.time() >= self._last_tick_time+(1 / self.tick_rate):
+            for i in self.entities:
+                i.tick()
+
+            time_off = (self._last_tick_time + (1/self.tick_rate)) - time.time()
+            self._last_tick_time = time.time() - time_off
+
+            return True
+
+        return False
+
+
+    def _render_game(self):
+        for i in self.entities:
+            i.render()
--- test/GSM_test.py
+++ test/GSM_test.py
@@ -1,16 +1,49 @@
 import unittest
-from game_state_manager import *
+import time
+from src.game_state_manager import *
 
 class GSMTestCase(unittest.TestCase):
     def setUp(self):
         self.gsm = GameStateManager()
 
-    def test_set_state(self):
-        self.gsm.
-
+    def test_state_changes(self):
+        # tests that state changes when conditions for change are met
+        self.assertEqual(self.gsm.get_state(), GameState.menu)
+        self.gsm.start_game()
+        self.gsm._tick()
+        self.assertEqual(self.gsm.get_state(), GameState.running)
+        self.gsm.lives = 0
+        self.gsm._tick()
+        self.assertEqual(self.gsm.get_state(), GameState.game_over)
 
     def test_kill_player(self):
+        # tests that player dies and lives go down
+        self.gsm.start_game()
+        self.gsm._tick()
+        old_lives = self.gsm.lives
+        self.gsm.get_player_ship().setHealth(0)
+        self.gsm._tick()
+        self.assertEqual(self.gsm.lives, old_lives-1)
 
+    def test_score_increase(self):
+        # tests that score goes up when an enemy dies
+        self.gsm.start_game()
+        self.gsm._tick()
+        old_score = self.gsm.game_score
+        self.gsm.get_enemies()[0].setHealth(0)
+        self.gsm.tick()
+        self.assertGreater(old_score, self.gsm.game_score)
+
+    def test_tick_timing(self):
+        # tests that ticking is locked to the tick rate
+
+        successful_ticks = 0
+        start_time = time.time()
+        while successful_ticks < 10:
+            if self.gsm._tick():
+                successful_ticks += 1
+
+        self.assertEqual((time.time()-start_time)//(1/self.gsm.tick_rate), successful_ticks)
 
     def tearDown(self):
         self.gsm = None
