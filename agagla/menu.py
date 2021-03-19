85b23a8a62a069d1ec4b40e4220a9a4c434b497f
Ingot <moneybagsx@yahoo.com>
2021-03-18 20:11:47 -0400

    Add the menu
    - Added some menu code.
    - Moved some of the code from main into the menu constructor instead.

--- agagla/menu.py
+++ agagla/menu.py
@@ -1,5 +1,25 @@
+import pygame
+
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
+
+
 class Menu:
 
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.font_large = pygame.font.Font("../data/fonts/arcadeclassic.regular.ttf", 40)
+        self.font_small = pygame.font.Font("../data/fonts/arcadeclassic.regular.ttf", 30)
+
     def render(self):
-        # TODO: render menu here
+        textsurface = self.font_large.render('Agagla', False, (255, 255, 255))
+        self.screen.blit(textsurface, ((WINDOW_WIDTH / 2) - (textsurface.get_width() / 2), (WINDOW_HEIGHT / 2) - 200))
+
+        textsurface = self.font_small.render('Press    fire    to    start', False, (255, 255, 255))
+        self.screen.blit(textsurface, ((WINDOW_WIDTH / 2) - (textsurface.get_width() / 2), (WINDOW_HEIGHT / 2) - 100))
+
+        from game_state_manager import GameStateManager
+        if GameStateManager.get_instance().get_input_manager().get_fire():
+            GameStateManager.get_instance().start_game()
+
         return None
