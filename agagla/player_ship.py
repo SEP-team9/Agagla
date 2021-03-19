68048d3f631747a38ce44402a737c1e57a355277
JJ Ryan <jjryan111@yahoo.com>
2021-03-19 14:29:25 -0400

    Created Player Ship class

--- agagla/player_ship.py
+++ agagla/player_ship.py
@@ -1,17 +1,36 @@
-# placeholder stub for unit testing, delete this when making the actual class
+import pygame
+from agagla.game_state_manager import GameStateManager
+from agagla.projectile import Projectile
+from agagla.ship import Ship
 
-class PlayerShip:
-    def __init__(self):
+VELOCITY = 5
+
+class PlayerShip(Ship):
+    def __init__(self, x, y):
         self.health = 2
+        self.rect = Rect(x, y, 10, 10)
 
-    def set_health(self, health):
-        self.health = health
+    def move(self):
+        im = GameStateManager.get_input_manager()
+        l= im.get_left()
+        r = im.get_right()
+        if (l):
+            self.x -= VELOCITY
+        elif (r):
+            self.x += VELOCITY
+        return self.x
 
-    def get_health(self):
-        return self.health
-
-    def tick(self):
-        return None
+    def fire_projectile(self):
+        im = GameStateManager.get_input_manager()
+        f = im.get_fire()
+        if (f):
+            proj = Projectile(self)
+            proj.position = (self.x, self.y-10)
+            return proj
+        else:
+            return None
 
     def render(self):
+        pygame.Surface.blit(self.rect)
         return None
+
