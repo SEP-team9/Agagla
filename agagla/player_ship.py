b48bd6d46c36f907a67cf3594c268b0165d04400
JJ Ryan <jjryan111@yahoo.com>
2021-03-21 16:03:59 -0400

    Refactored to match class diagram

--- agagla/player_ship.py
+++ agagla/player_ship.py
@@ -1,35 +1,34 @@
 import pygame
-from pygame.math import Vector2
-from agagla import game_state_manager
-from agagla.projectile import Projectile
+from agagla import game_state_manager as gsm
 from agagla.ship import Ship
 
 VELOCITY = 5
 INITHEALTH = 2
+INITX = (1920 / 2)
+INITY = (1080 - 30)
 
 class PlayerShip(Ship):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.set_health(INITHEALTH)
         self.velocity = VELOCITY
-        self.set_pos(x, y)
+        self.set_pos(INITX, INITY)
         self.rect = pygame.Rect(self.get_pos()[0], self.get_pos()[1], 10, 10)
-
-    def fire_projectile(self):
-        proj = Projectile(self.get_pos()[0], self.get_pos()[1] - 10)
-        return proj
 
     def render(self):
         pygame.Surface.blit(self.rect)
 
     def tick(self):
-        im = game_state_manager.GameStateManager.get_input_manager()
+        im = gsm.GameStateManager.get_input_manager()
         left = im.get_left()
         right = im.get_right()
         fire = im.get_fire()
+        self._calculateMovement(left, right, fire)
+
+    def _calculateMovement(self, left, right, fire):
         if left:
             self.move(-self.velocity, 0)
         elif right:
             self.move(self.velocity, 0)
         elif fire:
-            self.fire_projectile()
+            self.spawn_projectile((0, -10), 0)
--- tests/test_Player_Ship.py
+++ tests/test_Player_Ship.py
@@ -1,15 +1,17 @@
 import unittest
 import pygame
 from agagla.player_ship import PlayerShip
-from agagla.projectile import Projectile
 
 class test_player_ship(unittest.TestCase):
 
     def setUp(self):
         pygame.init()
-        self.ps = PlayerShip(500, 500)
+        self.ps = PlayerShip(0,0)
 
     def test_move(self):
+        # Test initial position
+        self.assertEqual(self.ps.get_pos(), (960, 1050))
+
         # Tests that the player ship moves right
         oldx = self.ps.get_pos()[0]
         oldy = self.ps.get_pos()[1]
@@ -23,11 +25,6 @@ class test_player_ship(unittest.TestCase):
         self.ps.move(-self.ps.velocity, 0)
         self.assertEqual(self.ps.get_pos()[0], oldx-self.ps.velocity)
         self.assertEqual(self.ps.get_pos()[1], oldy)
-
-    def test_fire_projectile(self):
-        # Tests that a new projectile appears
-        newproj = self.ps.fire_projectile()
-        self.assertIsInstance(newproj, Projectile)
 
     def tearDown(self):
         self.ps = None
