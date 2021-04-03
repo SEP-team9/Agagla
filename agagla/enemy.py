6cb739f2be3bed604619cf22e55557a7ba00c952
Klayton Smith II <motoklay@gmail.com>
2021-04-03 13:19:06 -0400

    blit correction/alignment (#18)
    Co-authored-by: Sean Radatz <SeanRadatz@gmail.com>

--- agagla/enemy.py
+++ agagla/enemy.py
@@ -8,12 +8,16 @@ import os
 class Enemy(ship.Ship):
     def __init__(self, position):
         super().__init__(position, Vector2(10, 10))
-        enemy1 = os.path.join('../data/enemy1.png')
-        self.image = pygame.image.load(enemy1)
+        path = os.path.join('../data/enemy1.png')
+        enemy1 = pygame.image.load(path)
+        enemy1 = pygame.transform.scale(enemy1, (50, 50))
+        self.image = enemy1
         self.health = 1
         self.velocity = 1
         self.type = 1
-        self.rect = pygame.Rect(self.get_pos().x, self.get_pos().y, 10, 10)
+        self.rect = self.image.get_rect()
+        self.rect.x = self.get_pos().x
+        self.rect.y = self.get_pos().y
 
     def path(self):
         self._position.y = self._position.y + self.velocity
@@ -29,8 +33,7 @@ class Enemy(ship.Ship):
         return self.type
 
     def render(self):
-        self.rect = pygame.Rect(self.get_pos().x, self.get_pos().y, 10, 10)
-        pygame.draw.rect(pygame.display.get_surface(), (255, 255, 255), self.rect)
+        pygame.Surface.blit(pygame.display.get_surface(), pygame.transform.rotate(self.image, 180), (self.get_pos().x - 20, self.get_pos().y))
         return None
 
     def tick(self):
--- agagla/player_ship.py
+++ agagla/player_ship.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame
 from agagla import shared_objects
 from agagla.ship import Ship
@@ -7,21 +9,26 @@ VELOCITY = 5
 INIT_HEALTH = 1
 MAX_PROJECTILES = 3
 INIT_X = (1920 / 2)
-INIT_Y = (1080 - 30)
+INIT_Y = (1080 - 75)
 
 
 class PlayerShip(Ship):
     def __init__(self, position):
-        super().__init__(position, Vector2(10, 10))
+        super().__init__(position, Vector2(35, 35))
         self.set_health(INIT_HEALTH)
         self.velocity = VELOCITY
         self.set_pos(Vector2(INIT_X, INIT_Y))
-        self.rect = pygame.Rect(self.get_pos()[0], self.get_pos()[1], 10, 10)
+        path = os.path.join('../data/player.png')
+        player = pygame.image.load(path)
+        player = pygame.transform.scale(player, (50, 50))
+        self.image = player
+        self.rect = self.image.get_rect()
+        self.rect.x = self.get_pos()[0]
+        self.rect.y = self.get_pos()[1]
         self.firing = False
 
     def render(self):
-        self.rect = pygame.Rect(self.get_pos()[0], self.get_pos()[1], 10, 10)
-        pygame.draw.rect(pygame.display.get_surface(), (255, 255, 255), self.rect)
+        pygame.Surface.blit(pygame.display.get_surface(), self.image, (self.get_pos().x - 17, self.get_pos().y - 17))
 
     def tick(self):
         im = shared_objects.get_im()
