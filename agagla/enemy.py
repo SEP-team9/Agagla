98d27df01dac6d5ad8c6460093b1e5f46ac53479
Ingot <moneybagsx@yahoo.com>
2021-04-04 21:16:13 -0400

    Bug Fixes & Enemy Work
    - Refactored some enemy type code
    - Fixed a few bugs
    - Added unlimited enemy stages
    - Added some enemy movement and firing

--- agagla/enemy.py
+++ agagla/enemy.py
@@ -4,9 +4,10 @@ from agagla import ship
 from pygame.math import Vector2
 import os
 from enum import Enum
+from agagla import shared_objects
 
 
-class Type(Enum):
+class EnemyType(Enum):
     STANDARD = 1
     ASSAULT = 2
     REINFORCED = 3
@@ -14,56 +15,24 @@ class Type(Enum):
 
 
 class Enemy(ship.Ship):
-    #    def __init__(self, position):
-    #        super().__init__(position, Vector2(50, 50))
-    #        path = os.path.join('../data/enemy1.png')
-    #        enemy1 = pygame.image.load(path)
-    #        enemy1 = pygame.transform.scale(enemy1, (50, 50))
-    #        self.image = enemy1
-    #        self.health = 1
-    #        self.velocity = 1
-    #        self.type = 1
-    #        self.rect = self.image.get_rect()
-    #        self.rect.x = self.get_pos().x
-    #        self.rect.y = self.get_pos().y
-    #
-    #    def path(self):
-    #        self._position.y = self._position.y + self.velocity
-    #        self.rect = pygame.Rect(self.get_pos().x, self.get_pos().y, 10, 10)
-    #        return None
-    #
-    #    def fire(self):
-    #        if random.randint(1, 180) == 45:
-    #            self.spawn_projectile(Vector2(0, 10), 0)
-    #        return None
-    #
-    #    def get_type(self):
-    #        return self.type
-    #
-    #    def render(self):
-    #        pygame.Surface.blit(pygame.display.get_surface(), pygame.transform.rotate(self.image, 180),
-    #                            (self.get_pos().x - 20, self.get_pos().y))
-    #        return None
-    #
-    #    def tick(self):
-    #        self.path()
-    #        self.fire()
-    #        return None
-    #
-    def __init__(self, position):
-        if Type == 1:
+
+    def __init__(self, position, type):
+        self.type = type
+        self.current_fire_cooldown = 5
+        self.idle = True
+        if self.type == EnemyType.STANDARD:
             super().__init__(position, Vector2(50, 50))
             path = os.path.join('../data/enemy1.png')
             enemy1 = pygame.image.load(path)
             enemy1 = pygame.transform.scale(enemy1, (50, 50))
             self.image = enemy1
             self.health = 1
+            self.fire_cooldown = 10
             self.velocity = 1
-            self.type = 1
             self.rect = self.image.get_rect()
             self.rect.x = self.get_pos().x
             self.rect.y = self.get_pos().y
-        elif Type == 2:
+        elif self.type == EnemyType.ASSAULT:
             super().__init__(position, Vector2(50, 50))
             path = os.path.join('../data/enemy2.png')
             enemy2 = pygame.image.load(path)
@@ -71,11 +40,11 @@ class Enemy(ship.Ship):
             self.image = enemy2
             self.health = 1
             self.velocity = 2
-            self.type = 2
+            self.fire_cooldown = 15
             self.rect = self.image.get_rect()
             self.rect.x = self.get_pos().x
             self.rect.y = self.get_pos().y
-        elif Type == 3:
+        elif self.type == EnemyType.REINFORCED:
             super().__init__(position, Vector2(50, 50))
             path = os.path.join('../data/enemy3.png')
             enemy3 = pygame.image.load(path)
@@ -83,7 +52,7 @@ class Enemy(ship.Ship):
             self.image = enemy3
             self.health = 2
             self.velocity = 1
-            self.type = 3
+            self.fire_cooldown = 20
             self.rect = self.image.get_rect()
             self.rect.x = self.get_pos().x
             self.rect.y = self.get_pos().y
@@ -94,38 +63,37 @@ class Enemy(ship.Ship):
             enemy4 = pygame.transform.scale(enemy4, (50, 50))
             self.image = enemy4
             self.health = 3
+            self.fire_cooldown = 30
             self.velocity = 3
-            self.type = 4
             self.rect = self.image.get_rect()
             self.rect.x = self.get_pos().x
             self.rect.y = self.get_pos().y
 
     def path(self):
-        self._position.y = self._position.y + self.velocity
+        gsm = shared_objects.get_gsm()
+        # self._position.y = self._position.y + self.velocity
+        self.move(Vector2(-self.velocity if gsm.enemy_idle_left else self.velocity, 0))
         self.rect = pygame.Rect(self.get_pos().x, self.get_pos().y, 10, 10)
         return None
 
     def fire(self):
-        if Type == 1:
-            if random.randint(1, 100) == 25:
+        self.current_fire_cooldown = self.fire_cooldown
+        if self.type == EnemyType.STANDARD:
+            self.spawn_projectile(Vector2(0, 10), 0)
+        if self.type == EnemyType.ASSAULT:
+            for x in range(3):
                 self.spawn_projectile(Vector2(0, 10), 0)
-        if Type == 2:
-            if random.randint(1, 50) == 25:
-                for x in range(3):
-                    self.spawn_projectile(Vector2(0, 10), 0)
-        if Type == 3:
-            if random.randint(1, 200) == 25:
+        if self.type == EnemyType.REINFORCED:
+            self.spawn_projectile(Vector2(0, 10), 0)
+            self.spawn_projectile(Vector2(0, 10), 45)
+            self.spawn_projectile(Vector2(0, 10), -45)
+        if self.type == EnemyType.ELITE:
+            for x in range(5):
                 self.spawn_projectile(Vector2(0, 10), 0)
                 self.spawn_projectile(Vector2(0, 10), 45)
                 self.spawn_projectile(Vector2(0, 10), -45)
-        if Type == 4:
-            if random.randint(1, 25) == 25:
-                for x in range(5):
-                    self.spawn_projectile(Vector2(0, 10), 0)
-                    self.spawn_projectile(Vector2(0, 10), 45)
-                    self.spawn_projectile(Vector2(0, 10), -45)
-                    self.spawn_projectile(Vector2(0, 10), 22)
-                    self.spawn_projectile(Vector2(0, 10), -22)
+                self.spawn_projectile(Vector2(0, 10), 22)
+                self.spawn_projectile(Vector2(0, 10), -22)
         return None
 
     def get_type(self):
@@ -134,9 +102,33 @@ class Enemy(ship.Ship):
     def render(self):
         pygame.Surface.blit(pygame.display.get_surface(), pygame.transform.rotate(self.image, 180),
                             (self.get_pos().x - 20, self.get_pos().y))
+
+        # pygame.draw.line(pygame.display.get_surface(), (255, 0, 0), self.get_pos(), self.get_line_of_sight(), 3)
+
         return None
+
+    def should_fire(self):
+        if self.current_fire_cooldown > 0: return False
+        gsm = shared_objects.get_gsm()
+        player_ship = gsm.get_player_ship()
+        if player_ship is None: return False
+
+        closest_ship = None
+        closest_y = 10000000
+        for ship in gsm.get_ships():
+            if ship is self: continue
+            if ship.get_pos().y < self.get_pos().y: continue
+            if ship.get_pos().y < closest_y and abs(ship.get_pos().x - self.get_pos().x) <= ship.get_size().x:
+                closest_y = ship.get_pos().y
+                closest_ship = ship
+
+        return closest_ship is player_ship
 
     def tick(self):
         self.path()
-        self.fire()
+        if self.current_fire_cooldown > 0: self.current_fire_cooldown -= 1
+        if self.should_fire(): self.fire()
         return None
+
+    def is_idle(self):
+        return self.idle
