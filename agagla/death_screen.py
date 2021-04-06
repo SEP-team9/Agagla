d43097c68048096c97a1117280bc8681f1b1338b
JJ Ryan <jjryan111@yahoo.com>
2021-04-06 13:40:26 -0400

    Basic high scores functionality is there

--- agagla/death_screen.py
+++ agagla/death_screen.py
@@ -5,7 +5,7 @@ from agagla import shared_objects
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
 WAIT_TIME_TO_END = 10
-
+TOP_LINE = (WINDOW_HEIGHT / 2) - 300
 
 class DeathScreen:
 
@@ -30,26 +30,27 @@ class DeathScreen:
                 self.hs_rank = i
                 break
 
+
     def render(self):
 
         self.screen.fill((0, 0, 0))
         text_surface = self.font_large.render('Game Over', False, (255, 255, 255))
-        self.screen.blit(text_surface, ((WINDOW_WIDTH / 2) - (text_surface.get_width() / 2), (WINDOW_HEIGHT / 2) - 100))
+        self.screen.blit(text_surface, ((WINDOW_WIDTH / 2) - (text_surface.get_width() / 2), (WINDOW_HEIGHT ) - 300))
 
         time_ms = time.time() * 1000.0
 
         if time_ms - self.last_time > 500.0:
             self.blink = ~self.blink
             self.last_time = time_ms
-
+        self.display_scores()
         if self.hs_rank < 10:
             text_surface = self.font_small.render("HIGH SCORE!", False, (255, 255, 255))
-            self.screen.blit(text_surface, (((WINDOW_WIDTH - text_surface.get_width()) / 2), (WINDOW_HEIGHT / 2) - 350))
+            self.screen.blit(text_surface, (((WINDOW_WIDTH - text_surface.get_width()) / 2), (WINDOW_HEIGHT / 2) - 450))
 
-            text_surface = self.font_small.render(self.name + (self.ch if self.blink else '    '),
+            text_surface = self.font_small.render("{:<20} {:^25} {:>20}".format(self.name, (self.ch if self.blink else '    '),''),
                                                   False,
                                                   (255, 255, 255))
-            self.screen.blit(text_surface, (((WINDOW_WIDTH-text_surface.get_width()) / 2), (WINDOW_HEIGHT / 2) - 300))
+            self.screen.blit(text_surface, ((((WINDOW_WIDTH-text_surface.get_width()) / 2)), TOP_LINE + 50 * (self.hs_rank)))
 
             self.char_selection()
 
@@ -58,6 +59,21 @@ class DeathScreen:
             if self.hs_rank <= 10:
                 self.hsdb.add_high_score(self.name, self.score)
             self.gsm.submitted_hs()
+
+    def display_scores(self):
+        gutter = " "
+        for i in range(0, self.hs_rank):
+            hs_formatted = "{:<20} {:^25} {:>20}".format(self.h_scores[i][1], gutter, str(self.h_scores[i][2]))
+            text_surface = self.font_small.render(hs_formatted, False, (255, 255, 255))
+            self.screen.blit(text_surface, (((WINDOW_WIDTH - text_surface.get_width()) / 2), TOP_LINE + 50*i))
+        if self.hs_rank <10:
+            hs_formatted = "{:<20} {:^25} {:>20}".format('',gutter, str(self.score))
+            text_surface = self.font_small.render(hs_formatted, False, (255, 255, 255))
+            self.screen.blit(text_surface, (((WINDOW_WIDTH - text_surface.get_width()) / 2), TOP_LINE + 50 * self.hs_rank))
+        for i in range(self.hs_rank+1, len(self.h_scores)):
+            hs_formatted = "{:<20} {:^25} {:>20}".format(self.h_scores[i-1][1], gutter, str(self.h_scores[i-1][2]))
+            text_surface = self.font_small.render(hs_formatted, False, (255, 255, 255))
+            self.screen.blit(text_surface, (((WINDOW_WIDTH - text_surface.get_width()) / 2), TOP_LINE + 50*(i)))
 
     def char_selection(self):
         if self.im.get_right():
