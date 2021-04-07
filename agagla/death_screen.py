bdd57b8b259d5aeb2fb20f7330adfecd893cd781
Klayton Smith II <motoklay@gmail.com>
2021-04-06 21:10:15 -0400

    Disp hs (#27)
    * Basic high scores functionality is there
    
    * Titrated some positioning
    
    * Added max name length, cleaned up some stuff
    
    * Fixed bug where wrong name associated to score
    
    Co-authored-by: JJ Ryan <jjryan111@yahoo.com>

--- agagla/death_screen.py
+++ agagla/death_screen.py
@@ -2,7 +2,10 @@ import pygame
 import time
 from agagla import shared_objects
 WAIT_TIME_TO_END = 10
-
+TOP_LINE = (WINDOW_HEIGHT / 2) - (WINDOW_HEIGHT * 0.3)
+LEFT_COLUMN = (WINDOW_WIDTH / 2) - (WINDOW_WIDTH * 0.05)
+RIGHT_COLUMN = (WINDOW_WIDTH / 2) + (WINDOW_WIDTH * 0.05)
+MAX_NAME_LENGTH = 10
 
 class DeathScreen:
 
@@ -27,35 +30,52 @@ class DeathScreen:
                 self.hs_rank = i
                 break
 
+
     def render(self):
         shared_objects.get_bg().render()
         text_surface = self.font_large.render('Game Over', False, (255, 255, 255))
-        self.screen.blit(text_surface, ((shared_objects.get_window_width() / 2) - (text_surface.get_width() / 2), (shared_objects.get_window_height() / 2) - 100))
+
+        self.screen.blit(text_surface, ((WINDOW_WIDTH / 2) - (text_surface.get_width() / 2), (WINDOW_HEIGHT ) - 300))
 
         time_ms = time.time() * 1000.0
 
         if time_ms - self.last_time > 500.0:
             self.blink = ~self.blink
             self.last_time = time_ms
-
+        self.display_scores()
         if self.hs_rank < 10:
-            text_surface = self.font_small.render("HIGH SCORE!", False, (255, 255, 255))
 
-            self.screen.blit(text_surface, (((shared_objects.get_window_width() - text_surface.get_width()) / 2), (shared_objects.get_window_height() / 2) - 350))
+            text_surface = self.font_small.render("HIGH SCORE ACHIEVED!", False, (255, 255, 255))
+            self.screen.blit(text_surface, (((WINDOW_WIDTH - text_surface.get_width()) / 2), (WINDOW_HEIGHT / 2) - 450))
 
-            text_surface = self.font_small.render(self.name + (self.ch if self.blink else '    '),
+            text_surface = self.font_small.render(self.name + (self.ch if self.blink else '_'),
                                                   False,
                                                   (255, 255, 255))
-            
-            self.screen.blit(text_surface, (((shared_objects.get_window_width()-text_surface.get_width()) / 2), (shared_objects.get_window_height() / 2) - 300))
+
+            self.screen.blit(text_surface, (LEFT_COLUMN, TOP_LINE + 50 * self.hs_rank))
 
             self.char_selection()
 
-        if time.time() - self.time_of_last_key > WAIT_TIME_TO_END:
+        if (time.time() - self.time_of_last_key > WAIT_TIME_TO_END) or len(self.name) == MAX_NAME_LENGTH:
             # print(self.name)
             if self.hs_rank <= 10:
                 self.hsdb.add_high_score(self.name, self.score)
             self.gsm.submitted_hs()
+
+    def display_scores(self):
+        for i in range(0, self.hs_rank):
+            hs_name = self.font_small.render(self.h_scores[i][1], False, (255, 255, 255))
+            hs_score = self.font_small.render(str(self.h_scores[i][2]), False, (255, 255, 255))
+            self.screen.blit(hs_name, (LEFT_COLUMN, TOP_LINE + 50 * i))
+            self.screen.blit(hs_score, (RIGHT_COLUMN, TOP_LINE + 50 * i))
+        if self.hs_rank <10:
+            text_surface = self.font_small.render(str(self.score), False, (255, 255, 255))
+            self.screen.blit(text_surface, (RIGHT_COLUMN, TOP_LINE + 50 * self.hs_rank))
+        for i in range(self.hs_rank, len(self.h_scores)):
+            hs_name = self.font_small.render(self.h_scores[i][1], False, (255, 255, 255))
+            hs_score = self.font_small.render(str(self.h_scores[i][2]), False, (255, 255, 255))
+            self.screen.blit(hs_name, (LEFT_COLUMN, TOP_LINE + 50 * (i + 1)))
+            self.screen.blit(hs_score, (RIGHT_COLUMN, TOP_LINE + 50 * (i + 1)))
 
     def char_selection(self):
         if self.im.get_right():
