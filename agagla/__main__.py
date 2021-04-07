7a4e33bfe75b6e60783cd387b2c6b70c8bc2e330
klayton smith <motoklay@gmail.com>
2021-04-06 20:28:22 -0400

    winners dont...

--- agagla/__main__.py
+++ agagla/__main__.py
@@ -1,6 +1,8 @@
 from agagla import game_state_manager
 import pygame
 from agagla import shared_objects
+import os
+import time
 
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
@@ -20,10 +22,14 @@ def init():
 
     pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-
 if __name__ == '__main__':
 
     init()
+
+    winners = pygame.image.load(os.path.join("../data/winners.png"))
+    pygame.Surface.blit(pygame.display.get_surface(), winners, ((WINDOW_WIDTH-winners.get_width())/2, (WINDOW_HEIGHT-winners.get_height())/2))
+    pygame.display.update()
+    time.sleep(5)
 
     while gsm.get_state() != game_state_manager.GameState.exit:
         gsm.game_loop()
